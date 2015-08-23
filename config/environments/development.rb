fea82b67e113ecc59013f487de0ee926a763c178
Bibek Shrestha <bibekshrestha@gmail.com>
2015-08-23 11:17:17 +0200

    Mailer uses password from dotenv files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,12 +17,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
+  ap "GMAIL USERNAME PASSWORD #{ENV["GMAIL_USERNAME"]}, #{ENV["GMAIL_PASSWORD"]}"
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
-    user_name:            "bibekshrestha",
-    password:             "bojoovwcyjbisrht",
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       "plain",
     enable_starttls_auto: true }
 
