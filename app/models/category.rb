a76f3f0b65c4548c6e6e6808d6f667b148ea925b
Bibek Shrestha <bibekshrestha@gmail.com>
2015-08-23 12:13:33 +0200

    Fix category list query, remove expiry check

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
   has_many :jobs
 
   def latest_jobs
-    jobs.where(expired: false).where.not(published_at: nil).order(published_at: :desc)
+    jobs.where.not(published_at: nil).order(published_at: :desc)
   end
 end
